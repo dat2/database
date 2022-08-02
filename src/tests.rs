3555814ed23d64a4b2ac55eb0ddd63c410ecf723
Nick Dujay <nick.dujay@instacart.com>
2022-08-01 23:01:07 -0400

    update tests

--- src/tests.rs
+++ src/tests.rs
@@ -1,30 +1,19 @@
-use crate::table::TABLE_MAX_ROWS;
 use anyhow::Result;
 use assert_cmd::Command;
+
 
 #[test]
 fn inserts_and_selects() -> Result<()> {
     let mut cmd = Command::cargo_bin("database")?;
     let input = vec!["insert 1 'user' 'user@example.com'", "select", ".exit\n"];
     let output = ["Executed.", "(1, 'user', 'user@example.com')", "Exiting.\n"];
-    cmd.write_stdin(input.join("\n"))
+    let file = assert_fs::NamedTempFile::new("database.db")?;
+    cmd
+        .arg(file.path())
+        .write_stdin(input.join("\n"))
         .assert()
         .success()
         .stdout(output.join("\n"));
-    Ok(())
-}
-
-#[test]
-fn fails_at_max_pages() -> Result<()> {
-    let mut cmd = Command::cargo_bin("database")?;
-    let mut input: Vec<_> = (1..=TABLE_MAX_ROWS + 1)
-        .map(|i| format!("insert {i} 'user{i}' 'person{i}@email.com'", i = i))
-        .collect();
-    input.push(".exit\n".to_owned());
-    cmd.write_stdin(input.join("\n"))
-        .assert()
-        .success()
-        .stdout(predicates::str::contains("Table is full."));
     Ok(())
 }
 
@@ -51,7 +40,11 @@ fn allows_inserting_max_length() -> Result<()> {
         ),
         "Exiting.\n".to_owned(),
     ];
-    cmd.write_stdin(input.join("\n"))
+
+    let file = assert_fs::NamedTempFile::new("database.db")?;
+    cmd
+        .arg(file.path())
+        .write_stdin(input.join("\n"))
         .assert()
         .success()
         .stdout(output.join("\n"));
